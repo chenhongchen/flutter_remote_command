ae093c097420670884894297c7aa78d3f0a61a4e
chenhongchen <412130100@qq.com>
2023-10-23 13:39:58 +0800

    设置android端channel

--- android/src/main/java/com/chc/flutter_remote_command/FlutterRemoteCommandPlugin.java
+++ android/src/main/java/com/chc/flutter_remote_command/FlutterRemoteCommandPlugin.java
@@ -3,36 +3,54 @@ package com.chc.flutter_remote_command;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** FlutterRemoteCommandPlugin */
-public class FlutterRemoteCommandPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+/**
+ * FlutterRemoteCommandPlugin
+ */
+public class FlutterRemoteCommandPlugin implements FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    private EventChannel eventChannel;
+    EventChannel.EventSink mEventSink;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_remote_command");
-    channel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_remote_command/method");
+        channel.setMethodCallHandler(this);
+        eventChannel = new EventChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_remote_command/event");
+        eventChannel.setStreamHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        eventChannel.setStreamHandler(null);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onListen(Object arguments, EventChannel.EventSink events) {
+        mEventSink = events;
+    }
+
+    @Override
+    public void onCancel(Object arguments) {
+        mEventSink = null;
+    }
 }
